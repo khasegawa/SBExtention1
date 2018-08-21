2154ab4209ba1632ea8459d88f208f10fef615c3
K H <khasegawa@users.noreply.github.com>
2018-08-21 11:04:58 +0900

    Add project files.

---
+++ SBExtention1/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using Microsoft.SmallBasic.Library;
+
+/// <summary>
+/// 拡張入出力クラスです。
+/// </summary>
+[SmallBasicType]
+public static class ExtIO
+{
+
+#if true
+    public static Primitive Trainer()
+    {
+        var result = new Primitive();
+        result[1] = "Trainer1";
+        result[2] = "Trainer2";
+        return result;
+
+    }
+#endif
+
+    /// <summary>
+    /// 入力ボックス
+    /// </summary>
+    /// <param name="prompt">プロンプト文字列</param>
+    /// <param name="title">タイトル</param>
+    /// <returns>入力された文字列</returns>
+    public static Primitive InpBox(Primitive prompt, Primitive title)
+    {
+        return Interaction.InputBox(prompt, title);
+    }
+
+    /// <summary>
+    /// メッセージボックス
+    /// </summary>
+    /// <param name="message">表示するメッセージ</param>
+    /// <param name="title">タイトル</param>
+    /// <returns>メッセージ文字列</returns>
+    public static Primitive MsgBox(Primitive message, Primitive title)
+    {
+        MessageBox.Show(message, title, MessageBoxButtons.OK, MessageBoxIcon.None, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+        return message;
+    }
+}
